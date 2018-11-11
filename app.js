eed146f331f99f922035f39e5afd24ab6b8a9d0a
Zeztron <pateltharsh@gmail.com>
2018-11-11 17:40:30 -0500

    Added book to list and clear field feature

---
+++ app.js
@@ -0,0 +1,54 @@
+// Book Contructor
+function Book(title, author, isbn) {
+    this.title = title,
+    this.author = author,
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add book to list
+UI.prototype.addBookToList = function(book) {
+    const list = document.getElementById("book-list");
+    // Create tr element
+    const row = document.createElement("tr");
+    // Insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+}
+
+// Clearthe fields after adding book to List
+UI.prototype.clearFields = function() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+}
+
+// Event Listeners
+document.getElementById("book-form").addEventListener("submit", function(e) {
+    // Get form values
+    const title = document.getElementById("title").value,
+        author = document.getElementById("author").value,
+        isbn = document.getElementById("isbn").value;
+
+    // Instantiate the book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate the UI
+    const ui = new UI();
+
+    // Add book to List
+    ui.addBookToList(book);
+
+    // Clear fields
+    ui.clearFields();
+
+    e.preventDefault();
+});
