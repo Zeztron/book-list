add008c1e5e9511b06201c9a7d54ded8d40481ea
Zeztron <pateltharsh@gmail.com>
2018-11-11 18:27:08 -0500

    Added validation and alerts

--- app.js
+++ app.js
@@ -24,6 +24,26 @@ UI.prototype.addBookToList = function(book) {
     list.appendChild(row);
 }
 
+// Show alert
+UI.prototype.showAlert = function(message, className) {
+    // create a div
+    const div = document.createElement("div");
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector(".container");
+    // Get form
+    const form = document.querySelector("#book-form");
+    // Insert alert
+    container.insertBefore(div, form);
+    // Timeout after 3 secs
+    setTimeout(function() {
+        document.querySelector(".alert").remove();
+    }, 3000);
+}
+
 // Clearthe fields after adding book to List
 UI.prototype.clearFields = function() {
     document.getElementById("title").value = "";
@@ -44,11 +64,28 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
     // Instantiate the UI
     const ui = new UI();
 
-    // Add book to List
-    ui.addBookToList(book);
+    // Validate
+    if (title === "" || author === "" || isbn === "") {
+        // Error alert
+        ui.showAlert("Please fill in all fields", "error");
+    } else {
+        // Add book to List
+        ui.addBookToList(book);
 
-    // Clear fields
-    ui.clearFields();
+        // Clear fields
+        ui.clearFields();
+    }
 
     e.preventDefault();
 });
+
+
+
+
+
+
+
+
+
+
+
